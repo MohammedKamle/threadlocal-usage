f1d9a29fcad5c38e23f532297a163e63b97f7249
MohammedKamle <66168802+MohammedKamle@users.noreply.github.com>
2023-11-07 15:25:05 +0530

    Update AndroidApp.java

--- src/main/java/AndroidApp.java
+++ src/main/java/AndroidApp.java
@@ -9,10 +9,8 @@ import java.util.concurrent.TimeUnit;
 
 public class AndroidApp {
 
-    String userName = System.getenv("LT_USERNAME") == null ?
-            "username" : System.getenv("LT_USERNAME"); //Add username here
-    String accessKey = System.getenv("LT_ACCESS_KEY") == null ?
-            "accessKey" : System.getenv("LT_ACCESS_KEY"); //Add accessKey here
+    String userName = "mohammadk";
+    String accessKey = "rakcBoBYHiy8BW7osVi4N1LGYjgJhRfAwvL1pPUvwCA1wfNChd";
 
     public String gridURL = "@mobile-hub.lambdatest.com/wd/hub";
 
