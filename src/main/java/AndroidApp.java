885f486ef50f6b64124fdfc8d35b07c4087d4d5c
MohammedKamle <66168802+MohammedKamle@users.noreply.github.com>
2024-02-05 14:53:22 +0530

    Update AndroidApp.java

--- src/main/java/AndroidApp.java
+++ src/main/java/AndroidApp.java
@@ -10,7 +10,7 @@ import java.util.concurrent.TimeUnit;
 public class AndroidApp {
 
     String userName = "mohammadk";
-    String accessKey = "rakcBoBYHiy8BW7osVi4N1LGYjgJhRfAwvL1pPUvwCA1wfNChd";
+    String accessKey = "gkrzT0iFKjDjehXpMTznxM1lHIZXSYjV3H8Ntk0s2rCUJJO3WU";
     String builName = System.getenv("LT_BUILD_NAME") == null ?
             "accessKey" : System.getenv("LT_BUILD_NAME"); //Add accessKey here
 
